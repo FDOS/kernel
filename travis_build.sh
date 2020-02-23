5ecfdd572ce074af080db02965573d45f6065115
Andrew Bird <ajb@spheresystems.co.uk>
2020-02-23 11:38:00 +0000

    Travis: Build and test with both GCC and Watcom
    Watcom compiler is downloaded and extracted if necessary. On Travis this
    will be always, but if you build locally multiple times using
    ./travis_build, it will use the previous watcom installation.
    
    [fixes #18]

--- travis_build.sh
+++ travis_build.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
 
-make all COMPILER=gcc
+if [ -z "${TRAVIS_BUILD_DIR}" ] ; then
+  TRAVIS_BUILD_DIR=$(pwd)
+fi
+echo TRAVIS_BUILD_DIR is \"${TRAVIS_BUILD_DIR}\"
 
+# Output directory
+rm -rf _output
+mkdir _output
+
+# GCC
+git clean -x -d -f -e _output -e _watcom -e ow-snapshot.tar.gz
+make all COMPILER=gcc
+mv -i bin/KGC*.map bin/KGC*.sys _output/.
+
+# Watcom
+if [ ! -d _watcom ] ; then
+  [ -f ow-snapshot.tar.gz ] || wget https://github.com/open-watcom/open-watcom-v2/releases/download/Current-build/ow-snapshot.tar.gz
+
+  mkdir _watcom
+  (cd _watcom && tar -xf ../ow-snapshot.tar.gz)
+fi
+
+export PATH=$TRAVIS_BUILD_DIR/bin:$PATH:$TRAVIS_BUILD_DIR/_watcom/binl64
+export WATCOM=$TRAVIS_BUILD_DIR/_watcom
+
+git clean -x -d -f -e _output -e _watcom -e ow-snapshot.tar.gz
+make all COMPILER=owlinux
+mv -i bin/KWC*.map bin/KWC*.sys _output/.
