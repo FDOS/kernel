5b85f5fe6392e37c67aa2676e7fa0be0fd004b23
Andrew Bird <ajb@spheresystems.co.uk>
2020-02-23 11:38:00 +0000

    Travis: Build and test with both GCC and Watcom
    Watcom compiler is downloaded and extracted if necessary. On Travis this
    will be always, but if you build locally multiple times using
    ./travis_build, it will use the previous watcom installation.
    
    [fixes #18]

--- travis_test.sh
+++ travis_test.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-if [ -f kernel/kernel.sys ] ; then
-  echo Kernel has been built
-  exit 0
+KVER=8632
+
+if [ ! -f _output/KGC${KVER}.sys ] ; then
+  echo GCC built kernel not present
+  exit 1
 fi
 
-exit 1
+if [ ! -f _output/KWC${KVER}.sys ] ; then
+  echo Watcom built kernel not present
+  exit 1
+fi
+
+echo GCC and Watcom kernels have all been built
+ls -l _output/K*
+exit 0
