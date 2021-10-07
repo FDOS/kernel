7479889e79ceb33179374c4febd102624afcbcce
Andrew Bird <ajb@spheresystems.co.uk>
2021-10-07 23:11:57 +0100

    CI: Split into two output directories

--- ci_test.sh
+++ ci_test.sh
@@ -2,16 +2,16 @@
 
 KVER=8632
 
-if [ ! -f _output/KGC${KVER}.sys ] ; then
+if [ ! -f _output/gcc/KGC${KVER}.sys ] ; then
   echo GCC built kernel not present
   exit 1
 fi
 
-if [ ! -f _output/KWC${KVER}.sys ] ; then
+if [ ! -f _output/wc/KWC${KVER}.sys ] ; then
   echo Watcom built kernel not present
   exit 1
 fi
 
 echo GCC and Watcom kernels have all been built
-ls -l _output/K*
+find _output -ls
 exit 0
