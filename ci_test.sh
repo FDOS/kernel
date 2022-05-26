04353fe9e4ea2c25bae2354eee43e302308193ee
C. Masloch <pushbx@ulukai.org>
2022-05-26 21:52:58 +0200

    ci_test: do simple boot tests

--- ci_test.sh
+++ ci_test.sh
@@ -14,4 +14,17 @@ fi
 
 echo GCC and Watcom kernels have all been built
 find _output -ls
+
+cd test
+if ! ./test.sh ../_output/gcc/KGC${KVER}.sys diskgc bootgc 'boot gcc: '
+then
+  echo GCC boot test failed
+  exit 2
+fi
+if ! ./test.sh ../_output/wc/KWC${KVER}.sys diskwc bootwc 'boot wc: '
+then
+  echo OpenWatcom boot test failed
+  exit 2
+fi
+cd ..
 exit 0
